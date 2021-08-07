9dcc56c69f08f999b11538fc8e9eb8a34f2bbb78
daniel-w-johnson <daniel.w.johnson@ndsu.edu>
2021-08-07 15:51:08 -0500

    initial push of program

--- Form2.cs
+++ Form2.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OsuOverlay
+namespace KeystrokeCounter
 {
     public partial class Form2 : Form
     {
--- FormProfile.Designer.cs
+++ FormProfile.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace OsuOverlay
+﻿namespace KeystrokeCounter
 {
     partial class FormProfile
     {
